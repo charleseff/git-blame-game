ba7674efc5e7803135df842190a23182c2982f55
Charles Finkel <cff@stripe.com>
2016-04-25 18:11:28 -0400

    Fix relative paths

--- lib/git-blame-game/git_blame_game.rb
+++ lib/git-blame-game/git_blame_game.rb
@@ -1,6 +1,8 @@
+require 'open3'
+
 class GitBlameGame
-  def initialize(path_to_file, opts={})
-    @path_to_file = path_to_file
+  def initialize(p, opts={})
+    @path_to_file = set_path_to_file(p)
     @sha = !opts[:sha].nil? ? opts[:sha] : 'HEAD'
   end
 
@@ -11,7 +13,7 @@ class GitBlameGame
       sha_to_show = show_git_blame_and_prompt_for_sha
 
       p_flush("\n")
-      files_changed = `git show --pretty="format:" --name-only #{sha_to_show}`.split("\n")[1..-1]
+      files_changed = git_run(%Q{show --pretty="format:" --name-only #{sha_to_show}})[0].split("\n")
 
       @path_to_file = prompt_for_file(files_changed, sha_to_show)
       @sha = "#{sha_to_show}^"
@@ -19,8 +21,19 @@ class GitBlameGame
   end
 
   private
+  def git_run(cmd)
+    git_cmd = "git #{cmd}"
+    ret = Open3.capture3(git_cmd, :chdir => git_root_path)
+    exitstatus = ret[2].exitstatus
+    if exitstatus != 0
+      $stderr.print("Error: got exit status #{exitstatus} running `#{git_cmd}`\n")
+      exit 1
+    end
+    ret
+  end
+
   def show_git_blame_and_prompt_for_sha
-    git_blame_out = `#{git_blame_cmd}`
+    git_blame_out = git_run(git_blame_cmd)[0]
     exit $?.exitstatus unless $?.success?
     sha_list = get_sha_list(git_blame_out)
     print_git_blame_and_prompt
@@ -49,12 +62,12 @@ class GitBlameGame
   end
 
   def print_git_blame_and_prompt
-    system git_blame_cmd
+    p_flush git_run(git_blame_cmd)[0]
     p_flush "\n" + simple_prompt
   end
 
   def git_blame_cmd
-    "git blame #{@sha} -- #{@path_to_file}"
+    "blame #{@sha} -- #{@path_to_file}"
   end
 
   GIT_BLAME_REGEX = /(.+?) /
@@ -92,7 +105,7 @@ class GitBlameGame
   end
 
   def print_file_prompt(files, sha)
-    system "git show #{sha}"
+    p_flush(git_run("show #{sha}")[0])
     print("\n")
     files.each_with_index do |file, index|
       line = sprintf("%3d) #{file}", index+1)
@@ -134,6 +147,21 @@ class GitBlameGame
 
   def orange_color(s)
     s.colorize(:color => :yellow, :background => :black)
+  end
+
+  def git_root_path
+    @git_root_path ||= "#{`git rev-parse --show-toplevel`.strip}/"
+  end
+
+  # sets the path to be relative to the git root
+  def set_path_to_file(path_to_file)
+    @path_to_file =
+      if path_to_file.start_with?('/')
+        path_to_file.gsub(git_root_path, '')
+      else
+        # puts "ENV['PWD']=#{ENV['PWD']}, "
+        "#{ENV['PWD'].gsub(git_root_path, '')}/#{path_to_file}"
+      end
   end
 
 end
